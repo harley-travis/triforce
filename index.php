a90ede7746f28b2599e741b334ce5c4e65820524
harley-travis <harley.travis2@gmail.com>
2016-10-31 01:40:57 -0600

    commented out unnecessary php functdions

--- index.php
+++ index.php
@@ -299,73 +299,8 @@
     <li><span onclick="stoneOptions()" >Stone Options</span></li>
     <li><span onclick="cutOptions()" >Cut Options</span></li>
 </ul>
-<?php
 
-// declare the ring variable
-//           insert the selections into the array
-
-// switch -> make selections move to next case 
-// switch -> image selections for each case
-
-// execute case function
-
-// function: insert selection into array
-//           change image
-//           insert into table / order
-//           move to next case
-
-// display images
-// select image
-// image triggers switch
-
-
-
-
-$ring = array(
-    
-    'band' => array(
-                    '',
-                    '',
-                    ''
-                ),
-    'color' => array(
-                    '',
-                    '',
-                    ''
-                ),
-    'stone' => array(
-                    '',
-                    '',
-                    ''
-                ),
-    'cut' => array(
-                    '',
-                    '',
-                    ''
-                )
-);
-
-
-// switch to choose options. insert into array
-
-switch ($ring){
-    case 'option 1':
-        echo "you selected option 1";
-        $ring['band'][3] = 'turd band';
-        break;
-    case 'option 2':
-        echo "you selected option 2";
-        break;
-    default:
-        echo "select an option";
-        
-}
-
-
-// view array uncomment code below
-//echo '<pre>', print_r($ring, true), '</pre>';
-
-?>
+<p>Select and option</p>
 
 <button onclick="passArray()">Finished</button>
 
@@ -373,6 +308,54 @@ switch ($ring){
 
 
 
+
+<?php
+//
+//$ring = array(
+//    
+//    'band' => array(
+//                    '',
+//                    '',
+//                    ''
+//                ),
+//    'color' => array(
+//                    '',
+//                    '',
+//                    ''
+//                ),
+//    'stone' => array(
+//                    '',
+//                    '',
+//                    ''
+//                ),
+//    'cut' => array(
+//                    '',
+//                    '',
+//                    ''
+//                )
+//);
+
+
+// switch to choose options. insert into array
+
+//switch ($ring){
+//    case 'option 1':
+//        echo "you selected option 1";
+//        $ring['band'][3] = 'turd band';
+//        break;
+//    case 'option 2':
+//        echo "you selected option 2";
+//        break;
+//    default:
+//        echo "select an option";
+        
+//}
+
+
+// view array uncomment code below
+//echo '<pre>', print_r($ring, true), '</pre>';
+
+?>
 
 
 
